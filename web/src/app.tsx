914411729003aa565427818392c9833bb92dbb3b
João Fábio Marques <jfmarques2468@gmail.com>
2024-09-12 19:22:10 -0300

    Aula 02 Completa

---
+++ web/src/app.tsx
@@ -0,0 +1,13 @@
+import { Dialog } from "./components/ui/dialog";
+import { CreateGoal } from "./components/create-goal";
+import { EmptyGoals } from "./components/empty-goals";
+
+export function App() {
+  return (
+    <Dialog>
+      <EmptyGoals />
+
+      <CreateGoal />
+    </Dialog>
+  );
+}
